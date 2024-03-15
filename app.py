34735ff4ee71d26af002a6c7247a6e6f1f7ea478
Kaionobre <kaionobrega00@gmail.com>
2024-03-15 17:51:31 -0300

    Adicionando conteúdo da aula

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/alura-cursos/pandas-conhecendo-a-biblioteca/main/base-de-dados/aluguel.csv'
+
+base_dados = pd.read_csv(url, sep=';')
+
+#print(base_dados.head(10)) mostra as 5 primeiras linhas por padrão, e as N por parâmetro
+
+#print(base_dados.shape) mostra as linhas e colunas do seu DataFrame
+
+#print(base_dados.columns) mostra as informações de todas as colunas do seu DataFrame
+
+#print(base_dados.info()) mostra as colunas, os dados não nulos e o tipo de dado de cada coluna
+
+
